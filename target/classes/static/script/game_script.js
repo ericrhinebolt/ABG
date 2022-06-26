f490101fdcd0b8a0fb4314ea32984632f1f141e7
Eric Rhinebolt <55140312+ericrhinebolt@users.noreply.github.com>
2022-06-26 17:01:34 -0400

    Add files via upload

---
+++ target/classes/static/script/game_script.js
@@ -0,0 +1,60 @@
+const gameTitle = document.querySelector(".gameTitle");
+const gameId = document.querySelector(".gameId");
+const gameSummary = document.querySelector(".gameSummary");
+const gameReleaseDate = document.querySelector(".gameReleaseDate");
+const gamePrice = document.querySelector(".gamePrice");
+const gameWebsite = document.querySelector(".gameWebsite");
+const gameDescription = document.querySelector(".gameDescription");
+const gameRequirementsMin = document.querySelector(".gameRequirementsMin");
+const gameRequirementsRec = document.querySelector(".gameRequirementsRec");
+const gameDev = document.querySelector(".gameDev");
+const gamePub = document.querySelector(".gamePub");
+const gameLegal = document.querySelector(".gameLegal");
+const newReview = document.getElementById("newReview");
+const newFavorite = document.getElementById("newFavorite");
+const favAppId = document.querySelector(".favAppId");
+const newLink = document.createElement("a");
+const hiddenAppId = document.getElementById("hiddenAppId");
+
+
+let queryString = hiddenAppId.value;
+
+window.onload = function (){
+    fetch(`/steamapi/${queryString}`, requestOptions)
+        .then(response => response.json())
+        .then(result => test(result))
+        .catch(error => console.log('error', error));
+}
+
+function test(jsonResult) {
+    const { name, steam_appid, detailed_description, about_the_game, short_description, website, legal_notice } = jsonResult[queryString].data;
+
+    favAppId.value = steam_appid;
+    gameTitle.innerHTML = name;
+    gameId.innerHTML = "Steam ID: " + steam_appid;
+    gameReleaseDate.innerHTML = "Release Date: " + jsonResult[queryString].data.release_date.date;
+    gamePrice.innerHTML = "Price: " + jsonResult[queryString].data.price_overview.final_formatted + " " + jsonResult[queryString].data.price_overview.currency;
+    newLink.setAttribute("href", website);
+    newLink.setAttribute("class", "gameWebsiteLink");
+    gameWebsite.appendChild(newLink);
+    newLink.innerHTML = website;
+    gameSummary.innerHTML = short_description;
+    gameDescription.innerHTML = about_the_game;
+    gameRequirementsMin.innerHTML = jsonResult[queryString].data.pc_requirements.minimum;
+    gameRequirementsRec.innerHTML = jsonResult[queryString].data.pc_requirements.recommended;
+    gameDev.innerHTML = "Developer: " + jsonResult[queryString].data.developers[0];
+    gamePub.innerHTML = "Publisher: " + jsonResult[queryString].data.publishers[0];
+    gameLegal.innerHTML = legal_notice;
+
+    console.log(jsonResult[queryString].data.name);
+}
+
+newReview.addEventListener("click", function (){
+    window.location.href = `../../reviews/new_review/${queryString}`
+})
+
+newFavorite.addEventListener("click", function (){
+    window.location.href = "/add_favorite"
+})
+
+// http://localhost:8080/games/game_info/374850
---
+++ target/classes/static/script/index.js
@@ -0,0 +1,89 @@
+const heroLogin = document.querySelector(".heroLogin");
+const heroRegister = document.querySelector(".heroRegister");
+const newsAppId = document.querySelectorAll(".newsAppId");
+const newsDate = document.querySelectorAll(".newsDate");
+const newsGameName = document.querySelectorAll(".newsGameName")
+const newsTitle = document.querySelectorAll(".newsTitle");
+const newsContent = document.querySelectorAll(".newsContent");
+const newLink = document.createElement("a");
+const newsLink = document.querySelectorAll(".newsLink")
+const favImg1 = document.querySelectorAll(".favImg1");
+const favImg2 = document.querySelectorAll(".favImg2");
+const favImg3 = document.querySelectorAll(".favImg3");
+const favImgLink1 = document.querySelectorAll(".favImgLink1")
+const favImgLink2 = document.querySelectorAll(".favImgLink2")
+const favImgLink3 = document.querySelectorAll(".favImgLink3")
+const imgDiv = document.querySelectorAll(".imgDiv");
+
+let requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+window.onload = function () {
+
+
+    for (let i = 0; i < newsAppId.length; i++) {
+        let newsAppIdvalue = newsAppId[i].value;
+        console.log(newsAppId);
+        console.log(newsAppIdvalue)
+        fetch(`/steamapi/news/${newsAppIdvalue}`, requestOptions)
+            .then(response => response.json())
+            .then(result => assign(result))
+            .catch(error => console.log('error', error));
+
+        function assign(jsonResult) {
+            const {appid} = jsonResult.appnews;
+            let title, url, contents, date;
+            if(jsonResult.appnews.newsitems[0]) {
+                date = jsonResult.appnews.newsitems[0].date;
+                title = jsonResult.appnews.newsitems[0].title;
+                url = jsonResult.appnews.newsitems[0].url;
+                contents = jsonResult.appnews.newsitems[0].contents;
+            }
+            console.log(title)
+            // newsParent.appendChild(newDateDiv);
+            newsDate[i].innerHTML = new Date(date * 1000);
+
+            fetch(`/steamapi/${appid}`, requestOptions)
+                .then(response => response.json())
+                .then(result => subAssign(result))
+                .catch(error => console.log('error', error))
+
+            function subAssign(jsonResult) {
+                // newsParent.appendChild(newGameNameDiv);
+                console.log(i);
+                newsGameName[i].innerHTML = jsonResult[appid].data.name;
+                favImg1[i].setAttribute("src", jsonResult[appid].data.screenshots[0]?.path_thumbnail)
+                favImg2[i].setAttribute("src", jsonResult[appid].data.screenshots[1]?.path_thumbnail)
+                favImg3[i].setAttribute("src", jsonResult[appid].data.screenshots[2]?.path_thumbnail)
+                favImgLink1[i].setAttribute("href", jsonResult[appid].data.screenshots[0]?.path_full)
+                favImgLink2[i].setAttribute("href", jsonResult[appid].data.screenshots[1]?.path_full)
+                favImgLink3[i].setAttribute("href", jsonResult[appid].data.screenshots[2]?.path_full)
+                imgDiv[i].setAttribute("style", "grid-row:" + (i + 2) + "/ " + (i + 3));
+            }
+
+            // newsParent.appendChild(newTitleDiv);
+            newsTitle[i].innerHTML = title;
+            // newsParent.appendChild(newContentsDiv)
+            newsContent[i].innerHTML = contents;
+            newsLink[i].setAttribute("href", url);
+            newsLink[i].innerHTML = "Click here for more information"
+            // newsContent[i].lastChild.innerHTML("For more information, click here " + url);
+
+
+        }
+    }
+}
+
+
+if (heroLogin != null) {
+    heroLogin.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = "/login";
+    })
+    heroRegister.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = "/register";
+    })
+}
